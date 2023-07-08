aefbac34b71d709383993d4ed137b4a75f518b08
Diego Cotacio <diego.cotacio@correounivalle.edu.co>
2023-07-08 08:07:51 -0500

    credentials

--- src/connections/bq_conect.py
+++ src/connections/bq_conect.py
@@ -11,7 +11,7 @@ def connect_to_bigquery():
     # Load BigQuery credentials from the service_account.json file
     #credentials = service_account.Credentials.from_service_account_info(credentials_json)
 
-    credentials = service_account.Credentials.from_service_account_file('src\connections\protean-fabric-386717-d6a21dd66382.json')
+    credentials = service_account.Credentials.from_service_account_file('src/connections/protean-fabric-386717-d6a21dd66382.json')
 
     # Connect to the BigQuery API using the credentials
     client = bigquery.Client(credentials=credentials)
